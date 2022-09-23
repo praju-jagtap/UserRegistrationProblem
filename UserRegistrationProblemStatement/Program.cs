3fcd7b7967e4ebec926afbcb96e81d5ce194f1fb
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-23 14:09:46 +0530

    [Prajakta] Add: Display Welcome Message

---
+++ UserRegistrationProblemStatement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationProblemStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User Registration Problem Statement for Regex Validation");
+        }
+    }
+}
